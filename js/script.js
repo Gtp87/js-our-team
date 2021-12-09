8e77314c673f1004201e37d2c0dc72d7ba9ae7a8
Gtp87 <fabio.giannitrapani@gmail.com>
2021-12-09 16:30:09 +0100

    end bonus

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Ciao ragazzi,
+//   esercizio di oggi: Our Team
+// nome repo: js - our - team
+// In allegato trovate un layout di base e un file js in cui è definito un array di oggetti che rappresentano i membro del team.
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Prendendo come riferimento la card di esempio presente nell’html, stampiamo dinamicamente una card per ogni membro del team.
+
 const team = [
   {
     name: 'Wayne Barnett',
@@ -67,4 +74,42 @@ function printCards(array, container) {
 
 
 
+// BONUS:
+// Utilizziamo gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team: cliccando sul pulsante “add” viene creato un nuovo oggetto, il quale viene inserito nell’array iniziale, e viene stampata una nuova card con tutte le informazioni inserite dall’utente.
+
+// richiamo funzione per stampare le card
+
+printCards(team, teamContainer);
+
+// seleziono gli elementi del dom
+
+let nameUser = document.getElementById('name');
+let roleUser = document.getElementById('role');
+let imageUser = document.getElementById('image');
+let addMember = document.getElementById('addMemberButton');
+
+// al click del bottone
+
+addMember.addEventListener('click', function() {
+  // seleziono i valori del form
+  let selectedName = nameUser.value;
+  let selectedRole = roleUser.value;
+
+  // usare path img
+  let selectedImage = imageUser.value;
+
+  // variabile dei valori selezionati
+
+  let object  = {
+    name: selectedName,
+    role: selectedRole,
+    image: selectedImage,
+
+  }
+
+  // inserisco valori in array
+  team.push(object);
+  console.log(team);
+  printCards(team, teamContainer);
+})
 
